d87ec3da72e0863b594b9b1b45e163a7af1909b2
DiegoOno <diego.ono1505@gmail.com>
2019-05-15 21:39:45 -0300

    Correcting errors

--- shm_t1/app.py
+++ shm_t1/app.py
@@ -2,19 +2,20 @@ import sys
 
 morse_dict = {
     # Letters
-    'A': '0111', 'B': '111000', 'C': '11101110',
-    'D': '11100', 'E': '0', 'F': '001110', 'G': '1111110',
-    'H': '0000', 'I': '00', 'J': '0111111111', 'K': '1110111',
-    'L': '011100', 'M': '111111', 'N': '1110', 'O': '111111111',
-    'P': '01111110', 'Q': '1111110111', 'R': '01110', 'S': '000',
-    'T': '111', 'U': '00111', 'V': '000111', 'W': '0111111',
-    'X': '11100111', 'Y': '1110111111', 'Z': '11111100',
+    'A': '10111', 'B': '111010101', 'C': '11101011101',
+    'D': '1110101', 'E': '1', 'F': '101011101',
+    'G': '111011101', 'H': '1010101', 'I': '101',
+    'J': '1011101110111', 'K': '111010111',
+    'L': '101110101', 'M': '1110111', 'N': '11101',
+    'O': '11101110111', 'P': '10111011101', 'Q': '1110111010111',
+    'R': '1011101', 'S': '10101', 'T': '111', 'U': '1010111',
+    'V': '101010111', 'W': '101110111', 'X': '11101010111',
+    'Y': '1110101110111', 'Z': '11101110101',
     # Numbers
-    '1': '0111111111111', '2': '00111111111', '3': '000111111111',
-    '4': '0000111', '5': '00000', '6': '1110000', '7': '111111000',
-    '8': '11111111100', '9': '1111111111110', '0': '111111111111111',
-    # Space
-    ' ': ' '}
+    '1': '10111011101110111', '2': '101011101110111',
+    '3': '10101011101110111', '4': '10101010111', '5': '101010101',
+    '6': '11101010101', '7': '1110111010101', '8': '111011101110101',
+    '9': '11101110111011101', '0': '1110111011101110111'}
 
 
 def get_morse_key(val):
@@ -26,20 +27,19 @@ def get_morse_key(val):
 
 
 def text_to_morse(morse_file, text):
+    morse_text = ''
     text = text.upper()
     for char in text:
-        morse_file.write(morse_dict[char])
-        morse_file.write('\n')
+        if not char == ' ':
+            morse_text += morse_dict[char]
+            morse_text += '000' 
+        else:
+            morse_text += '0000'
+    morse_text = morse_text[:-3]
 
+    morse_file.write(morse_text)
 
-def morse_to_text(text_file, morse_list):
-    string = ''
-
-    for i in range(0, len(morse_list)):
-        string += get_morse_key(morse_list[i])
-    print(string)
-    text_file.write(string)
-
+#def morse_to_text(text_file, morse_list):
 
 def main():
     file_path = sys.argv[1]
@@ -55,15 +55,11 @@ def main():
         text_to_morse(morse_file, text)
         morse_file.close()
         text_file.close()
+        # TO DO text to audio
 
     elif file_extension == 'morse':
         print('TXT and Audio.\n')
-        morse_list = [line.rstrip('\n') for line in open(file_path)]
-
-        # morse to text
-        text_file = open('text.txt', 'w')
-        morse_to_text(text_file, morse_list)
-        text_file.close()
+        
 
     elif file_extension == 'wav':
         print('TXT and Morse.\n')
