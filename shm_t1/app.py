87c4980372f99056991926129c3db465e0079f60
DiegoOno <diego.ono1505@gmail.com>
2019-06-05 20:01:36 -0300

    Audio to text and audio to morse

--- shm_t1/app.py
+++ shm_t1/app.py
@@ -1,6 +1,7 @@
 import sys
 from text_functions import text_to_morse, text_to_audio
 from morse_functions import morse_to_text, morse_to_audio
+from audio_functions import audio_to_morse, audio_to_text
 
 def main():
     file_path = sys.argv[1]
@@ -9,31 +10,48 @@ def main():
 
     if file_extension == 'txt':
         print('Morse and Audio.\n')
+        
+        # text -> morse code
         text_file = open(file_path, 'r')
         text = text_file.read()
-        # text -> morse code
         morse_file = open('out/code.morse', 'w')
         morse_code = text_to_morse(text)
         morse_file.write(morse_code)
+
+        # text -> audio
         text_to_audio(text)
         morse_file.close()
         text_file.close()
         
     elif file_extension == 'morse':
         print('TXT and Audio.\n')
+
         # morse -> text
         text_file = open('out/text.txt', 'w')
         morse_file = open(file_path, 'r')
         morse_code = morse_file.read()
         text = morse_to_text(morse_code)
         text_file.write(text)
+        text_file.close()
+
+        # morse -> text
         morse_to_audio(morse_code)
         morse_file.close()
-        text_file.close()   
-        
-        
+
     elif file_extension == 'wav':
         print('TXT and Morse.\n')
+        # audio -> text
+        text_file = open('out/text.txt', 'w')
+        text = audio_to_text(file_path)
+        text_file.write(text)
+        text_file.close()
+
+        #audio -> morse
+        morse_file = open('out/code.morse', 'w')
+        morse_code = audio_to_morse(file_path)
+        morse_file.write(morse_code)
+        morse_file.close()
+
     else:
         print('Wrong extension.\n')
 
--- shm_t1/audio_functions.py
+++ shm_t1/audio_functions.py
@@ -1,4 +1,27 @@
 import wave
+import struct
+import numpy as np
+from utils import duration, sampling_rate
+from morse_functions import morse_to_text
+
+def load_wave(audio_path):
+    
+    with wave.open(audio_path) as wave_file:
+        num_frames = wave_file.getnframes()
+        data = wave_file.readframes(num_frames)
+        data = struct.unpack(f'{num_frames}h', data)
+        return np.array(data)
 
 def audio_to_morse(audio_path):
-    return
+    loaded_wave = load_wave(audio_path)
+    generated_morse = ''
+    frames_per_code = int(sampling_rate * duration)
+    for i in range(int(frames_per_code / 2), len(loaded_wave), frames_per_code):
+        generated_morse += '1' if max(loaded_wave[range(i - 10, i + 10)], key = abs) != 0 else '0'
+	
+    print(generated_morse)
+    return generated_morse
+
+def audio_to_text(audio_path):
+
+    return morse_to_text(audio_to_morse(audio_path))
